1168880df294e44c36f8b2c0d1f6b12e63b1f85e
Emma Zheng <73250770+skkra0@users.noreply.github.com>
2025-02-22 12:41:23 -0500

    update auth

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -5,14 +5,35 @@ export default function LoginPage() {
   const CLIENT_ID = 'e79f3d3f007545a1a45f490cc789f63f';
   const REDIRECT_URI = 'http://localhost:5173/callback';
   const SCOPES = 'user-read-playback-state user-modify-playback-state user-read-currently-playing streaming';
-  const handleSpotifyLogin = () => {
+  const handleSpotifyLogin = async () => {
     const authUrl = new URL('https://accounts.spotify.com/authorize');
-    const params = {
-      response_type: 'code',
-      client_id: CLIENT_ID,
-      scope: SCOPES,
-      redirect_uri: REDIRECT_URI,
-    };
+    const generateRandomString = (length) => {
+      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const values = crypto.getRandomValues(new Uint8Array(length));
+      return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  }
+  const sha256 = async (plain) => {
+      const encoder = new TextEncoder()
+      const data = encoder.encode(plain)
+      return window.crypto.subtle.digest('SHA-256', data)
+  }
+  const base64encode = (input) => {
+      return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+  }
+  const codeVerifier = generateRandomString(64);
+  localStorage.setItem('code_verifier', codeVerifier);
+  const codeChallenge = base64encode(await sha256(codeVerifier));
+  const params = {
+    response_type: 'code',
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge,
+    redirect_uri: REDIRECT_URI,
+  };
 
     authUrl.search = new URLSearchParams(params).toString();
     window.location.href = authUrl.toString();
--- frontend/src/pages/RedirectPage.jsx
+++ frontend/src/pages/RedirectPage.jsx
@@ -1,25 +1,62 @@
 import { useEffect, useState } from 'react';
 export default function RedirectPage() {
     const SERVER_URL = 'http://localhost:3001';
+    const REDIRECT_URI = 'http://localhost:5173/callback';
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const CLIENT_ID = 'e79f3d3f007545a1a45f490cc789f63f';
     useEffect(() => {
         async function getAccessToken() {
             try{
                 const params = new URLSearchParams(window.location.search);
-                const response = await fetch(`${SERVER_URL}/auth?code=${params.get('code')}`, {
-                    method: 'POST',
+                const code = params.get('code');
+
+                const body = await fetch("https://accounts.spotify.com/api/token", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: new URLSearchParams({
+                        client_id: CLIENT_ID,
+                        grant_type: "authorization_code",
+                        code: code,
+                        redirect_uri: REDIRECT_URI,
+                        code_verifier: localStorage.getItem('code_verifier')
+                    })
                 });
-                window.sessionStorage.setItem('token', response.access_token);
+
+                if (!body.ok) {
+                    throw new Error("Failed to get access token");
+                }
+                const response = await body.json();
+                
+                const body_2 = await fetch(`${SERVER_URL}/auth/login`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        token: response.access_token,
+                        refresh_token: response.refresh_token,
+                        expires_in: response.expires_in
+                    })
+                });
+
+                if (!body_2.ok) {
+                    throw new Error("Failed to login");
+                }
+
+                const response_2 = await body_2.json();
+                localStorage.setItem('token', response_2.token);
                 setLoading(false);
-                window.location.href = '/';
+                window.location.href = "/";
             } catch (err) {
                 setLoading(false);
                 setError(err.message);
             }
         }
         getAccessToken();
-        console.log(window.sessionStorage.getItem('token'));
+        console.log(sessionStorage.getItem('token'));
     }, [])
 
     if (loading) return <p>Loading...</p>;
