fe333dc159d88e65cdae4d7f5c1317d73b963b03
juzhao <zhao1306@purdue.edu>
2025-02-21 23:18:31 -0600

    boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Route for the home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Route for the login page */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div class="font-bold">This is the home page. Hello world.</div>;
+}
+
+export default Home;
